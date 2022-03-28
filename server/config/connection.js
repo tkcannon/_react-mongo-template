272a3bbf5039ffde6ccb1db5a4860aa0ada6aa85
Taylor Cannon <92164891+tkcannon@users.noreply.github.com>
2022-03-28 11:18:51 -0600

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/encountertracker', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/<DataBase Name>', {
   useNewUrlParser: true,
 });
 
